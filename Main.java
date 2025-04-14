6e25ed49bcf1e41a92d3b0b3798bbad3cdbac984
Jefferson Meira <jefferson.sinfoniac@gmail.com>
2025-04-13 21:57:19 -0300

    acrescentando variaveis

---
+++ Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        String x, y, z;
+
+        Scanner n = new Scanner(System.in);
+
+        System.out.println("Digite seu nome");
+        x = n.nextLine();
+        System.out.println("Olá "+x);
+
+    }
+
+    
+}
